2335de7d57f38cfc373d13cfab83ca243d59b29a
mhkeller <code@mhkeller.com>
2018-07-10 10:24:23 -0400

    some table tweaks, connection format change

--- src/shared/connectPg.js
+++ src/shared/connectPg.js
@@ -1,9 +1,10 @@
 import pg from 'pg';
 
 export default function connectPg (event, constring) {
-  const pool = new pg.Pool(constring);
+  const pool = new pg.Pool({connectionString: constring});
 
   pool.on('error', err => {
+    console.error(err);
     event.sender.send('status', 'error', err.message);
   });
 
