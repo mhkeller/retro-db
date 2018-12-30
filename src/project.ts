aaebabb1d9e9267c862096ee7fe2f9874f8c8f66
mhkeller <code@mhkeller.com>
2018-12-30 00:50:21 -0500

    add view switcher

--- src/project.ts
+++ src/project.ts
@@ -1,8 +1,13 @@
 import App from './project/App.html';
+import Table from './components/views/Table.html';
 import { Store } from 'svelte/store.js';
 
 const store = new Store({
-	query: ''
+	query: '',
+	activeViewId: 'table_0',
+	views: [
+		{ name: 'Table', id: `table_0`, type: 'Table', component: Table }
+	]
 });
 
 const app = new App({ // eslint-disable-line no-unused-vars
