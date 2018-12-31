25a05118e820290627278b25cb3227cd53bdc0a1
mhkeller <code@mhkeller.com>
2018-12-30 20:25:24 -0500

    add some not working view management

--- src/project.ts
+++ src/project.ts
@@ -1,13 +1,33 @@
 import App from './project/App.html';
 import Table from './components/views/Table.html';
+import MapView from './components/views/Map.html';
 import { Store } from 'svelte/store.js';
+import ShortUniqueId from 'short-unique-id';
+const uid = new ShortUniqueId();
+
+// const tableId = `table_${uid.randomUUID(6)}`;
+
+const viewRegistry = [
+	{ name: 'Table', type: 'Table', component: Table },
+	{ name: 'Map', type: 'Map', component: MapView }
+];
+
+function genNewView (type) {
+	return Object.assign(
+		{ id: `${type}_${uid.randomUUID(6)}` },
+		viewRegistry.filter(d => d.type === type)[0]);
+}
+
+const tableView = genNewView('Table');
 
 const store = new Store({
 	query: '',
-	activeViewId: 'table_0',
+	activeViewId: tableView.id,
 	views: [
-		{ name: 'Table', id: `table_0`, type: 'Table', component: Table }
-	]
+		tableView
+	],
+	viewRegistry,
+	genNewView
 });
 
 const app = new App({ // eslint-disable-line no-unused-vars
